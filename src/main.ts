da6e3da891028659ca819b32efc4655de6887961
GitHubGM <galok0211@gmail.com>
2025-10-16 15:05:26 +0500

    changed about page

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { ref } from 'vue'
 import App from './App.vue'
 import router from './router'
 
@@ -12,3 +12,15 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+export default {
+  // `setup` is a special hook dedicated for the Composition API.
+  setup() {
+    const count = ref(0)
+
+    // expose the ref to the template
+    return {
+      count
+    }
+  }
+}
